04862f2c4e2a49b151cf4c2c42ef12f8124f2bfc
hari-bhandari <2012bhandari.ha@gmail.com>
2021-02-01 19:26:28 +0000

    added interface on square.ts

--- client/src/UI/ChessBoard/square.ts
+++ client/src/UI/ChessBoard/square.ts
@@ -1,13 +1,14 @@
+import ChessPiece from "./chessPiece";
 class Square {
     x:0|1|2|3|4|5|6|7
     y:0|1|2|3|4|5|6|7
     canvasCoord:number;
-    pieceOnThisSquare:any
-    constructor(x:0|1|2|3|4|5|6|7, y:0|1|2|3|4|5|6|7, pieceOnThisSquare:number, canvasCoord:any) {
+    pieceOnThisSquare:ChessPiece|null
+    constructor(x:0|1|2|3|4|5|6|7, y:0|1|2|3|4|5|6|7, pieceOnThisSquare:ChessPiece|null, canvasCoord:number) {
         this.x = x
         this.y = y
         this.canvasCoord = canvasCoord
         this.pieceOnThisSquare = pieceOnThisSquare
-    }a
+    }
 }
 export default Square
