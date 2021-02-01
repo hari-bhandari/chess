18fbd6d9291148209823bfd3200406d8f48b9ef0
hari-bhandari <2012bhandari.ha@gmail.com>
2021-02-01 20:52:55 +0000

    added peice and finnally made it working

---
+++ client/src/UI/ChessBoard/Piece.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Image} from "react-konva";
+import logo from '../../logo.svg'
+const Piece = (props:any) => {
+    return (
+        <Image x={100} y={200} image={undefined}/>
+    );
+};
+
+export default Piece;
