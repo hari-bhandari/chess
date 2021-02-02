678b23d7c60ad4f339ea23880572b0532be92eb0
hari-bhandari <2012bhandari.ha@gmail.com>
2021-02-02 21:43:45 +0000

    added pieces to the box

--- client/src/Piece.tsx
+++ client/src/Piece.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import file from './assets/bB.svg'
+import styled from "styled-components";
 interface props{
     piece:PieceInterface
 }
@@ -6,11 +8,20 @@ interface PieceInterface {
     type:string,
     color:string
 }
+const PieceContainer=styled.div`
+  width: 100%;
+  height: 100%;
+  img{
+    width: 100%;
+  }
+`
 const Piece:React.FC<props> = ({piece:{type,color}}) => {
+    const path=require(`./assets/${color.toLowerCase()}${type.toUpperCase()}.svg`).default
+    console.log(path)
     return (
-        <div>
-            {type}
-        </div>
+        <PieceContainer>
+            <img src={path} alt=""/>
+        </PieceContainer>
     );
 };
 
