0dc005a4a829fb03c85fbc6b03af122eb0731aa7
hari-bhandari <2012bhandari.ha@gmail.com>
2021-02-02 17:48:42 +0000

    restyling to make it look like a actual board

--- client/src/BoardSquare.tsx
+++ client/src/BoardSquare.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import Square from "./Square";
 import Piece from "./Piece";
+import styled from "styled-components";
 interface props{
     piece:any;
     black:boolean
 }
+const SquareContainer=styled.div`
+  width: 100%;
+  height: 100%;
+`
 const BoardSquare:React.FC<props> = ({piece,black}) => {
     return (
-        <div>
+        <SquareContainer>
             <Square black={black}>
                 {piece&&<Piece piece={piece}/>}
             </Square>
             
-        </div>
+        </SquareContainer>
     );
 };
 
--- client/src/Square.tsx
+++ client/src/Square.tsx
@@ -6,6 +6,8 @@ interface props{
 }
 
 const SquareContainer=styled.div<{isBlack:boolean}>`
+  width: 100%;
+  height: 100%;
   background-color: ${p => p.isBlack?p.theme.black:p.theme.white};
 `
 const Square:React.FC<props> = ({children,black}) => {
