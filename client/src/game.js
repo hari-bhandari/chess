3f0c09b33f37f799405af22dcdfb7d9283267678
hari-bhandari <2012bhandari.ha@gmail.com>
2021-02-03 11:46:16 +0000

    pieces can move now

--- client/src/game.js
+++ client/src/game.js
@@ -6,6 +6,9 @@ export const gameSubject=new BehaviorSubject({
     board:chess.board()
 });
 export function move(from,to){
-    
+    const legalMove=chess.move({from,to})
+    if(legalMove){
+        gameSubject.next({board: chess.board()})
+    }
 
 }
