2282926012125d807c169f5ce57200a84f0d682b
hari-bhandari <2012bhandari.ha@gmail.com>
2021-02-03 15:50:17 +0000

    added update game

--- client/src/game.js
+++ client/src/game.js
@@ -1,14 +1,22 @@
 import * as Chess from 'chess.js'
 import {BehaviorSubject} from "rxjs";
-
-const chess=new Chess();
+const promotion='rnb2bnr/pppPkppp/8/4p3/7q/8/PPPP1PPP/RNBQKBNR w KQ - 1 5'
+const chess=new Chess(promotion);
 export const gameSubject=new BehaviorSubject({
     board:chess.board()
 });
 export function move(from,to){
     const legalMove=chess.move({from,to})
     if(legalMove){
-        gameSubject.next({board: chess.board()})
+       updateGame()
     }
+
+
+}
+const updateGame=()=>{
+    const newGame={
+        board:chess.board()
+    }
+    gameSubject.next(newGame)
 
 }
