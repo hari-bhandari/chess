2c5cb5d778d567df3c9d442295ab9024789584e2
hari-bhandari <2012bhandari.ha@gmail.com>
2021-02-05 10:42:01 +0000

    more styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,29 +6,30 @@ import GlobalStyles from "./styles/GlobalStyle";
 import {gameSubject, initGame} from "./game";
 import Board from "./UI/Board";
 import 'react-responsive-modal/styles.css';
-import { Modal } from 'react-responsive-modal';
+import {Modal} from 'react-responsive-modal';
 import RestartGame from "./RestartGame";
+
 const Container = styled.div`
   min-height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: rgb(34,34,34);
+  background-color: rgb(34, 34, 34);
 
 `
 const BoardContainer = styled.div`
   width: 600px;
   height: 600px;
-  
+
 `
 
 function App() {
     const [board, setBoard] = useState<any>([])
-    const [isGameOver,setIsGameOver]=useState<boolean>(false)
-    const [result,setResult]=useState<string|null|undefined>(null)
+    const [isGameOver, setIsGameOver] = useState<boolean>(false)
+    const [result, setResult] = useState<string | null | undefined>(null)
     useEffect(() => {
         initGame()
-        const subscribe = gameSubject.subscribe(game =>{
+        const subscribe = gameSubject.subscribe(game => {
             setBoard(game.board)
             setIsGameOver(game.isGameOver)
             setResult(game.result)
@@ -36,18 +37,19 @@ function App() {
         return () => subscribe.unsubscribe()
     }, [])
     return (
+        <Container>
+
             <ThemeProvider theme={theme}>
                 <GlobalStyles/>
-                {!isGameOver?
-                    (        <Container>
-                        <BoardContainer>
-                    <Board board={board}/>
-                        </BoardContainer></Container>):(
-                        <Modal open={isGameOver} onClose={()=>setIsGameOver(false)}>
-
-    <RestartGame/>
-                        </Modal>)}
+                   <BoardContainer>
+                        <Board board={board}/>
+                    </BoardContainer>
+                <Modal open={isGameOver} onClose={() => setIsGameOver(false)} styles={{modal:{backgroundImage:"url(https://images2.alphacoders.com/984/thumb-350-984589.jpg)"}}}
+                >
+                    <RestartGame result={result}/>
+                </Modal>
             </ThemeProvider>
+        </Container>
     );
 }
 
--- client/src/RestartGame.tsx
+++ client/src/RestartGame.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import styled from "styled-components";
-const RestartContainer=styled.div`
- width: 100%;
+import White from './assets/wK.svg'
+import Black from './assets/bK.svg'
+
+const RestartContainer = styled.div`
+  width: 100%;
+  display: flex;
+
+  justify-items: center;
+  justify-content: center;
+  flex-direction: column;
+  h2 {
+    display: flex;
+    justify-items: center;
+    justify-content: center;
+    margin: 20px 40px;
+
+    img {
+      width: 50px;
+    }
+  }
   
+
 `
-const Button=styled.button`
+const Button = styled.button`
   position: relative;
   margin: 10px 50px;
   display: inline-block;
@@ -24,7 +43,8 @@ const Button=styled.button`
   border-radius: 0.75em;
   transform-style: preserve-3d;
   transition: transform 150ms cubic-bezier(0, 0, 0.58, 1), background 150ms cubic-bezier(0, 0, 0.58, 1);
-  &:before{
+
+  &:before {
     position: absolute;
     content: "";
     width: 100%;
@@ -38,21 +58,26 @@ const Button=styled.button`
     box-shadow: 0 0 0 2px #b18597, 0 0.625em 0 0 #ffe3e2;
     transform: translate3d(0, 0.75em, -1em);
     transition: transform 150ms cubic-bezier(0, 0, 0.58, 1), box-shadow 150ms cubic-bezier(0, 0, 0.58, 1);
-    &:hover{
+
+    &:hover {
       background: #ffe9e9;
       transform: translate(0, 0.25em);
     }
   }
-  &:hover{
+
+  &:hover {
     background: #ffe9e9;
     transform: translate(0, 0.25em);
   }
-  
+
 `
-const RestartGame = () => {
+const RestartGame:React.FC<{result:string | null | undefined}> = ({result}) => {
     return (
-        <RestartContainer >
+        <RestartContainer>
+            <h2>{result}</h2>
             <Button>Play Again</Button>
+            <Button>Restart game</Button>
+            <Button>see the end game</Button>
         </RestartContainer>
     );
 };
